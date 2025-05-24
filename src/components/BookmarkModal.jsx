d7b9f3695de205b86f3145ea438baff317c56935
Nigel Robinson <nigelrobinson17@gmail.com>
2025-05-23 23:47:22 -0600

    Ui Updates

--- src/components/BookmarkModal.jsx
+++ src/components/BookmarkModal.jsx
@@ -23,12 +23,12 @@ useEffect(()=>{
     getBookmarks();
 },[openModal])
     return ( 
-        <div className="max-w-[1200px] w-full h-full bg-slate-950 r rounded-lg relative z-30 p-5 overflow-y-scroll sm:h-3/4">
-            <button className="relative text-white cursor-pointer float-right mb-4" onClick={exitModal}><BsXCircleFill className="text-TrendingRed/80 hover:text-TrendingRed" size={25}/></button>
+        <div className="w-full h-full rounded-lg relative z-30 p-5 overflow-y-scroll">
+            <button className="relative text-white cursor-pointer float-right mb-4" onClick={exitModal}><BsXCircleFill className="text-white/80 hover:text-white" size={20}/></button>
             <div className="relative w-full h-auto flex items-center justify-center">
                 {
                     bookmarkedMovies.length === 0 ? <div className="relative flex flex-col gap-20 items-center"><h1 className="text-white/30 text-2xl font-bold text-center">Your Bookmark List is Currently Empty</h1><BsFilm className="text-white/10" size={100}></BsFilm></div> : 
-                    <div className="grid gap-4 grid-cols-1 sm:grid-cols-3">{
+                    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">{
                     bookmarkedMovies.map((bookmarkedMovie, index) => (
                         <Card key={index} {...bookmarkedMovie}/>
                     ))}
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -3,7 +3,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Star from "../assets/MovieAssets/star.svg";
 import { useScratch } from "react-use";
 import noPoster from "../assets/MovieAssets/No-Poster.png";
+import noPosterHorizontal from "../assets/MovieAssets/No-Poster-Horizontal.png";
 import { Link, useParams } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -23,12 +25,13 @@ const MovieDetails = () => {
     const {title, id} = useParams();
     // const movieObject = location.state?.movie;
     // const {id, title, genre_names} = movieObject;
-    const {adult, budget, origin_country, overview, popularity, production_countries, spoken_languages, belongs_to_collection, production_companies, poster_path, backdrop_path, release_date, revenue, runtime, status, tagline, vote_average, vote_count} = movieDetails;
+    const {adult, budget, origin_country, overview, genres, popularity, production_countries, spoken_languages, belongs_to_collection, production_companies, poster_path, backdrop_path, release_date, revenue, runtime, status, tagline, vote_average, vote_count} = movieDetails;
 
     const getCompanies = production_companies?.map((company) => company.name) || [];
     const getLanguages = spoken_languages?.map((language) => language.english_name) || [];
     const getCountries = production_countries?.map((country) => country.name) || [];
     const backdropV2 = belongs_to_collection?.backdrop_path || backdrop_path;
+    const getGenres = genres?.map((genre) => genre.name) || [];
 
     const trailerMatch = trailer.find((trailer) => trailer.name?.toLowerCase().includes("official trailer") && trailer.type === "Trailer" && trailer.site === "YouTube");
     const fallbackTrailer = trailer.find((trailer) => trailer.type === "Trailer" && trailer.site === "YouTube");
@@ -36,21 +39,27 @@ const MovieDetails = () => {
     const selectedTrailer = trailerMatch || fallbackTrailer || anyYoutubeVideo;
     const trailerKey = selectedTrailer?.key || null;
 
-    const getMovieDetails = async() => {
-    const endPoint = `${API_BASE_URL}/movie/${id}?${API_KEY}&language=en-US`;
-    const response = await fetch(endPoint, API_OPTIONS);
+    const getMovieDetails = async() => {   
+    try{
+        const endPoint = `${API_BASE_URL}/movie/${id}?${API_KEY}&language=en-US`;
+        const response = await fetch(endPoint, API_OPTIONS);
 
-    if(!response.ok){
-        throw new Error("Failed to fetch Movie Details");
+        if(!response.ok){
+            throw new Error("Failed to fetch Movie Details");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        setMovieDetails(data);
     }
-
-    const data = await response.json();
-    console.log("MOVIE DETAILS", data);
-    setMovieDetails(data);
-}
+    catch(error){
+        console.log(`${error}`);
+    }
+    }
 
 
     const getTrailers = async() => {
+    try{
         const endpoint = `${API_BASE_URL}/movie/${id}/videos?${API_KEY}&language=en-US`;
         const response = await fetch (endpoint, API_OPTIONS);
 
@@ -59,9 +68,11 @@ const MovieDetails = () => {
         }
 
         const data = await response.json();
-        console.log("Trailer DETAILS", data);
         setTrailer(data.results);
-        console.log("Trailer KEY?", trailerKey);
+    }
+    catch(error){
+        console.log(`${error}`);
+    }
     }
 
     const releaseDate = new Date(release_date);
@@ -81,12 +92,12 @@ const MovieDetails = () => {
 
     return ( 
         <div className="bg-slate-950 w-full h-auto flex items-center justify-center">
-            <div className="relative w-full max-w-[1400px] h-full my-10 flex flex-col gap-4">
+            <div className="relative w-full max-w-[1400px] h-full flex flex-col gap-4 sm:my-10">
                 <div className="relative flex flex-col gap-4 overflow-hidden p-6">
 
                 <div className="relative w-full h-auto flex justify-between z-10">
                     <h2 className="text-white text-2xl font-semibold sm:text-3xl">{title ? title : "N/A"}</h2>
-                    <div className="flex  gap-2 bg px-4 bg-slate-500/30 rounded-lg items-center justify-center">
+                    <div className="relative flex gap-2 px-4 bg-slate-900/40 rounded-lg items-center justify-center">
                         <img src={Star} className="w-[20px]" alt="star_svg" />
                         <h2 className="text-white text-sm font-semibold sm:text-lg">{vote_average ? `${vote_average.toFixed(1)}/10` : "N/A"}</h2>
                     </div>
@@ -97,16 +108,15 @@ const MovieDetails = () => {
                     <span className="text-white text-xl opacity-50">&#x2022;</span>
                     <h2 className="text-white text-sm font-semibold sm:text-base">{adult ? <span className="text-red-500">Restricted (R)</span> : <span className="text-green-500">General Audience <span className="text-white">(G)</span></span>}</h2>
                     <span className="text-white text-xl opacity-50">&#x2022;</span>
-                    <h2 className="text-white text-sm sm:text-base">2h 46m</h2>
+                    <h2 className="text-white text-sm sm:text-base">{runtime ? `${Math.floor(runtime / 60)}h ${runtime % 60}m` : "N/A"}</h2>
                 </div>
 
                 <div className="relative flex flex-col justify-between gap-4 w-full z-10 mb-10 sm:flex-row overflow-x-hidden">
-                    <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${backdropV2}`: `${noPoster}`} alt={`${title}-Poster`} className=" text-white w-full rounded-lg shadow-lg shadow-black/40 sm:w-1/2" />
+                    <img src={backdropV2 ? `https://image.tmdb.org/t/p/w500/${backdropV2}`: `${noPosterHorizontal}`} alt={`${title}-Poster`} className=" text-white w-full rounded-lg shadow-lg shadow-black/40 sm:w-1/2" />
                     <iframe className="w-full h-auto rounded-lg shadow-lg shadow-black/40 sm:1/2" src={`https://www.youtube.com/embed/${trailerKey}`} title="Movie Trailer" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </div>
                 <div className="w-full h-full absolute bg-black/40 z-[5] top-0 left-0"></div>
-                <img src={poster_path ? `https://image.tmdb.org/t/p/original/${backdrop_path}`: `${noPoster}`} alt={`${title}-Poster`} className=" text-white rounded-lg absolute w-full h-full top-0 left-0 object-cover z-0 shadow-lg saturate-100" />
-               
+                <img src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt={`${title}-Poster`} className=" text-white rounded-lg absolute w-full h-full top-0 left-0 object-cover z-0 shadow-lg saturate-100" />
                 </div>
                 <div className="relative flex flex-col items-center gap-4 justify-between p-4 h-auto sm:flex-row sm:items-start sm:p-10">
                     <div className="max-w-[1100px] w-full flex flex-col gap-10 justify-between mb-8">
@@ -116,7 +126,7 @@ const MovieDetails = () => {
                     </div>
                     <div className="relative grid grid-cols-[2fr_3fr] gap-10 w-fulljustify-between items-center sm:grid-cols-[1fr_4fr]">
                         <h1 className="text-white font-semibold text-xl">Genres</h1>
-                        {/* <p className="text-white flex-1">{genre_names.join(", ")}</p> */}
+                        <p className="text-white flex-1">{getGenres.join(", ")}</p>
                     </div>
                     <div className="relative grid grid-cols-[2fr_3fr] gap-10 w-full justify-between sm:grid-cols-[1fr_4fr]">
                         <h1 className="text-white font-semibold text-xl">Release date</h1>
@@ -151,7 +161,7 @@ const MovieDetails = () => {
                         <h3 className="text-white flex-1">{getCompanies.join(", ")}</h3>
                     </div>
                     </div>
-                     <Link to={"/"}><button className="bg-TrendingRed px-4 py-2 w-fit h-fit text-white flex items-center justify-center font-semibold rounded-md cursor-pointer">Visit Homepage</button></Link>
+                     <Link to={"/"}><button className="bg-TrendingRed px-4 py-2 w-fit h-fit text-white flex items-center justify-center font-semibold rounded-md cursor-pointer">Visit Homepage<BsArrowRight className="text-white"/></button></Link>
                 </div>
             </div>
         </div>
